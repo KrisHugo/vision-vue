70dd41deee16cef1513a95a16980ab2996142615
Kris Hugo <2676086616@qq.com>
2022-09-25 17:40:43 +0800

    modified:   src/assets/main.css 	modified:   src/components/HelloWorld.vue 	modified:   src/components/WelcomeItem.vue 	modified:   src/components/forum/modules/search.vue

--- src/components/forum/modules/search.vue
+++ src/components/forum/modules/search.vue
@@ -1,39 +1,38 @@
-
 <script lang="ts">
 //这里可以导入其他文件(比如:组件,工具js,第三方插件js,json文件,图片文件等等)
 //例如:import 组件名称 from '组件路径';
 
 export default {
-    components: {},
-    data() {
-        //这里存放数据
-        return {
-            input: ""
-        };
-    },
-    //监听属性 类似于data概念
-    computed: {},
-    //监控data中的数据变化
-    watch: {},
-    //方法集合
-    methods: {
+    // components: {},
+    // data() {
+    //     //这里存放数据
+    //     return {
+    //         input: ""
+    //     };
+    // },
+    // //监听属性 类似于data概念
+    // computed: {},
+    // //监控data中的数据变化
+    // watch: {},
+    // //方法集合
+    // methods: {
 
-    },
-    //生命周期 - 创建完成(可以访问当前this实例)
-    created() {
+    // },
+    // //生命周期 - 创建完成(可以访问当前this实例)
+    // created() {
 
-    },
-    //生命周期 - 挂载完成(可以访问DOM元素)
-    mounted() {
+    // },
+    // //生命周期 - 挂载完成(可以访问DOM元素)
+    // mounted() {
 
-    },
-    beforeCreate() { }, //生命周期 - 创建之前
-    beforeMount() { }, //生命周期 - 挂载之前
-    beforeUpdate() { }, //生命周期 - 更新之前
-    updated() { }, //生命周期 - 更新之后
-    beforeDestroy() { }, //生命周期 - 销毁之前
-    destroyed() { }, //生命周期 - 销毁完成
-    activated() { }, //如果页面有keep-alive缓存功能,这个函数会触发
+    // },
+    // beforeCreate() { }, //生命周期 - 创建之前
+    // beforeMount() { }, //生命周期 - 挂载之前
+    // beforeUpdate() { }, //生命周期 - 更新之前
+    // updated() { }, //生命周期 - 更新之后
+    // beforeDestroy() { }, //生命周期 - 销毁之前
+    // destroyed() { }, //生命周期 - 销毁完成
+    // activated() { }, //如果页面有keep-alive缓存功能,这个函数会触发
 }
 </script>
     
@@ -45,12 +44,25 @@ export default {
             <input type="text" placeholder="search...">
             <button type="submit"></button>
         </form>
-        <p class="subtle">order by</p>
-        <button type="button">Latest</button>
+        <div class="order">
+            <div class="subtle">order by</div>
+            <button type="button">Latest</button>
+        </div>
     </div>
 </template>
 
 <style lang="css" scoped>
+.order{
+    width: 100%;
+    margin: 4rem 0;
+}
+
+.subtle{
+    font: 1.25rem;
+    opacity: 0.5;
+    font-weight: bold;
+    float: left;
+}
 
 .searchBar {
     display: inline-block;
@@ -101,4 +113,5 @@ export default {
     font-family: FontAwesome;
     color: #324b4e;
 }
+
 </style>
